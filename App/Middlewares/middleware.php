c5b6a1d6318c04998eb73aec197106040fe07490
yrllanbrandao <yrllanbrandao@outlook.com>
2023-08-28 03:52:05 -0300

    fix(middleware): incorrect path

--- App/Middlewares/middleware.php
+++ App/Middlewares/middleware.php
@@ -1,45 +1,8 @@
 <?php
-    namespace App;
 
+$logged = isset($_SESSION['role']);
 
-class Middeware{
-    private $logged;
-
-    public function __construct(){
-        $logged = isset($_SESSION['role']);
-        $this -> setLogged($logged);
-    }
-    public function next(){
-    }
-    public static function handle(string $role){
-        
-        if($this -> getLogged())
-        {
-            switch($role){
-                case 'student':
-                    break;
-                case 'admin':
-                    break;
-                case 'professor':
-                    break;
-                default: 
-                header('Location: /login?error=403');
-                exit;
-                ;
-            }
-           
-        }
-        else{
-            header('Location: /login?error=403');
-        }
-        
-        
-
-    }
-    public function getLogged(){
-        return $this -> logged;
-    }
-    public function setLogged(boolean $value){
-        return $this -> $value;
-    }
+if (!$logged) {
+    header('Location: /login?error=403');
+    exit;
 }
--- App/Views/partials/menu.php
+++ App/Views/partials/menu.php
@@ -1,12 +1,38 @@
+   <?php
+   require_once '../App/Middlewares/middleware.php';
+
+   $role = $_SESSION['role'];
+?>
 <nav class="navbar">
    
    <ul class="navbar-list d-flex flex-column"> 
-            <li  class="navbar-item-header d-flex gap-3"><iconify-icon icon="fa-solid:user" style="color: white;" width="30" height="30"></iconify-icon> <?= $username ?> | <?= $role ?></li>
+
+<nav class="navbar">   
+   <ul class="navbar-list d-flex flex-column"> 
+   <li  class="navbar-item-header d-flex gap-3"><iconify-icon icon="fa-solid:user" style="color: white;" width="30" height="30"></iconify-icon> <?= $username ?> | <?= $role ?></li>
+           <?php
+           switch($role){
+            case 'student':
+               ?>
+              
            <li class="navbar-item"> <a href="/document-and-processes" ><iconify-icon icon="mdi:file-document" style="color: white;" width="25" height="25"></iconify-icon> Documentos/Processos</a></li>
            <li class="navbar-item"><iconify-icon icon="fa-solid:chalkboard-teacher" style="color: white;" width="25" height="25"></iconify-icon> Ensino</li>
-      
+               <?php
+                break;
+            case 'admin':
+               ?>
+   <li class="navbar-item"> <iconify-icon icon="fa:users" style="color: white;"></iconify-icon> Gerenciar usuários</li>
+               <?php
+                break;
+            case 'professor':
+                break;
+         }
+           
+           ?>
+             <li class="navbar-item"><a href="/logout" class="link link-light d-flex align-items-center gap-2"><iconify-icon icon="ion:exit" style="color: #FFFFFF;" width="25" height="25"></iconify-icon> Sair</a></li>
+   </ul>
 
-           <li class="navbar-item"><a href="/logout" class="link link-light d-flex align-items-center gap-2"><iconify-icon icon="ion:exit" style="color: #FFFFFF;" width="25" height="25"></iconify-icon> Sair</a></li>
+</nav>
           
    </ul>
 </nav>
